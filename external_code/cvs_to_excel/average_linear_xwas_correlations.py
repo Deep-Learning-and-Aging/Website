dcc166ad54533656dcfb2251cfdeca53029c2696
Théo VINCENT <theo.vincent@eleves.enpc.fr>
2021-03-24 12:11:10 +0100

    csv to excel std

--- external_code/cvs_to_excel/average_linear_xwas_correlations.py
+++ external_code/cvs_to_excel/average_linear_xwas_correlations.py
@@ -4,14 +4,15 @@ import numpy as np
 if __name__ == "__main__":
     for subset_method in ["All", "Union", "intersection"]:
         for correlation_type in ["Pearson", "Spearman"]:
-            correlations = pd.read_csv(
-                f"../../data/page6_LinearXWASCorrelations/average_correlations/Correlations_comparisons_{subset_method}_{correlation_type}.csv",
-                index_col=[0],
-            )
-            correlations.index = pd.MultiIndex.from_tuples(
-                correlations.index.str.split("_").tolist(), names=("organ_2", "organ_1")
-            )
+            for mean_or_std in ["", "_sd"]:
+                correlations = pd.read_csv(
+                    f"../../data/page6_LinearXWASCorrelations/average_correlations/Correlations{mean_or_std}_comparisons_{subset_method}_{correlation_type}.csv",
+                    index_col=[0],
+                )
+                correlations.index = pd.MultiIndex.from_tuples(
+                    correlations.index.str.split("_").tolist(), names=("organ_2", "organ_1")
+                )
 
-            correlations.to_excel(
-                f"../../data/page6_LinearXWASCorrelations/average_correlations/Correlations_comparisons_{subset_method}_{correlation_type}.xlsx"
-            )
+                correlations.swaplevel().to_excel(
+                    f"../../data/page6_LinearXWASCorrelations/average_correlations/Correlations{mean_or_std}_comparisons_{subset_method}_{correlation_type}.xlsx"
+                )
