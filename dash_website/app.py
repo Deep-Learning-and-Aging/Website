c8b350c09b8ccb566530ec1d90bd3c13672384eb
Théo <theo.vincent@eleves.enpc.fr>
2021-04-27 16:02:44 +0200

    Google analytisc

--- dash_website/app.py
+++ dash_website/app.py
@@ -12,3 +12,30 @@ APP = dash.Dash(
 )
 
 APP.config.suppress_callback_exceptions = True
+
+APP.index_string = """<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3S0TF0F5WG"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-3S0TF0F5WG');
+        </script>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>"""
